464891dfbabd01a6e1c2fa0de0bf18f44144cd32
Koh Tharoth <Kohtharoth@gmail.com>
2025-01-23 22:54:51 +0700

    fix bug

--- app/src/main/java/com/codelytical/muvyz/core/data/network/MovieApi.kt
+++ app/src/main/java/com/codelytical/muvyz/core/data/network/MovieApi.kt
@@ -1,287 +1,139 @@
+package com.codelytical.muvyz.core.data.network
 
-package com.codelytical.muvyz.home.presentation
+import com.codelytical.muvyz.BuildConfig.API_KEY
+import com.codelytical.muvyz.core.util.Constants.STARTING_PAGE_INDEX
+import com.codelytical.muvyz.genre.data.network.GenresResponse
+import com.codelytical.muvyz.home.data.network.CreditsResponse
+import com.codelytical.muvyz.home.data.network.MovieDetails
+import com.codelytical.muvyz.home.data.network.MoviesResponse
+import com.codelytical.muvyz.home.data.network.TvSeriesDetails
+import com.codelytical.muvyz.home.data.network.TvSeriesResponse
+import com.codelytical.muvyz.search.data.network.MultiSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.paging.cachedIn
-import androidx.paging.filter
-import com.codelytical.muvyz.core.util.Resource
-import com.codelytical.muvyz.genre.domain.model.Genre
-import com.codelytical.muvyz.genre.domain.usecase.GetMovieGenresUseCase
-import com.codelytical.muvyz.genre.domain.usecase.GetTvSeriesGenresUseCase
-import com.codelytical.muvyz.home.data.repository.MoviesRepository
-import com.codelytical.muvyz.home.data.repository.TvSeriesRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+interface   MovieApi {
+    @GET("trending/movie/day")
+    suspend fun getTrendingTodayMovies(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): MoviesResponse
 
-@HiltViewModel
-class HomeViewModel @Inject constructor(
-    private val moviesRepository: MoviesRepository,
-    private val seriesRepository: TvSeriesRepository,
-    private val getMovieGenresUseCase: GetMovieGenresUseCase,
-    private val getTvSeriesGenresUseCase: GetTvSeriesGenresUseCase,
-) : ViewModel() {
-    private val _homeUiState = MutableStateFlow(HomeUiState())
-    val homeUiState = _homeUiState.asStateFlow()
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): MoviesResponse
 
-    init {
-        getTrendingMovies(null)
-        getNowPayingMovies(null)
-        getUpcomingMovies(null)
-        getTopRatedMovies(null)
-        getPopularMovies(null)
-        getPopularTvSeries(null)
-        getMoviesGenres()
+    @GET("movie/upcoming")
+    suspend fun getUpcomingMovies(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): MoviesResponse
 
-        getAiringTodayTvSeries(null)
-        getTrendingTvSeries(null)
-        getOnTheAirTvSeries(null)
-        getTopRatedTvSeries(null)
-        getOnTheAirTvSeries(null)
-        getSeriesGenres()
-    }
+    @GET("movie/top_rated")
+    suspend fun getTopRatedMovies(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): MoviesResponse
 
-    /**
-     * Movies field
-     */
-    fun getTrendingMovies(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                trendingMovies = if (genreId != null) {
-                    moviesRepository.getTrendingMoviesThisWeek().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    moviesRepository.getTrendingMoviesThisWeek().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("movie/now_playing")
+    suspend fun getNowPlayingMovies(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): MoviesResponse
 
+    @GET("trending/tv/day")
+    suspend fun getTrendingTvSeries(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): TvSeriesResponse
 
-    fun getUpcomingMovies(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                upcomingMovies = if (genreId != null) {
-                    moviesRepository.getUpcomingMovies().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    moviesRepository.getUpcomingMovies().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("tv/top_rated")
+    suspend fun getTopRatedTvSeries(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): TvSeriesResponse
 
-    fun getTopRatedMovies(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                topRatedMovies = if (genreId != null) {
-                    moviesRepository.getTopRatedMovies().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    moviesRepository.getTopRatedMovies().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("tv/on_the_air")
+    suspend fun getOnTheAirTvSeries(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): TvSeriesResponse
 
-    fun getNowPayingMovies(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                nowPlayingMovies = if (genreId != null) {
-                    moviesRepository.getNowPlayingMovies().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    moviesRepository.getNowPlayingMovies().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("tv/popular")
+    suspend fun getPopularTvSeries(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): TvSeriesResponse
 
-    fun getPopularMovies(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                popularMovies = if (genreId != null) {
-                    moviesRepository.getPopularMovies().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    moviesRepository.getPopularMovies().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("tv/airing_today")
+    suspend fun getAiringTodayTvSeries(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): TvSeriesResponse
 
-    private fun getMoviesGenres() {
-        viewModelScope.launch {
-            when (val result = getMovieGenresUseCase()) {
-                is Resource.Success -> {
-                    _homeUiState.update {
-                        it.copy(
-                            moviesGenres = result.data ?: emptyList()
-                        )
-                    }
-                }
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): MovieDetails
 
-                is Resource.Error -> {
-                    //loadingError.value = result.message.toString()
-                }
+    @GET("tv/{tv_id}")
+    suspend fun getTvSeriesDetails(
+        @Path("tv_id") tvSeriesId: Int,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): TvSeriesDetails
 
-                else -> {}
-            }
-        }
-    }
+    @GET("movie/{movie_id}/credits")
+    suspend fun getMovieCredits(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): CreditsResponse
 
-    /**
-     * Tv Series
-     */
-    fun getTrendingTvSeries(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                trendingTvSeries = if (genreId != null) {
-                    seriesRepository.getTrendingThisWeekTvSeries().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    seriesRepository.getTrendingThisWeekTvSeries().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("tv/{tv_id}/credits")
+    suspend fun getTvSeriesCredits(
+        @Path("tv_id") tvSeriesId: Int,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): CreditsResponse
 
-    fun getOnTheAirTvSeries(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                onAirTvSeries = if (genreId != null) {
-                    seriesRepository.getOnTheAirTvSeries().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    seriesRepository.getOnTheAirTvSeries().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("credit/{credit_id}")
+    suspend fun getCreditDetails(
+        @Path("credit_id") creditId: Int,
+        @Query("api_key") apiKey: String = API_KEY,
+    )
 
-    fun getTopRatedTvSeries(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                topRatedTvSeries = if (genreId != null) {
-                    seriesRepository.getTopRatedTvSeries().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    seriesRepository.getTopRatedTvSeries().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("genre/movie/list")
+    suspend fun getMovieGenres(
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): GenresResponse
 
-    fun getAiringTodayTvSeries(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                airingTodayTvSeries = if (genreId != null) {
-                    seriesRepository.getAiringTodayTvSeries().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    seriesRepository.getAiringTodayTvSeries().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
+    @GET("genre/tv/list")
+    suspend fun getTvSeriesGenres(
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = "en"
+    ): GenresResponse
 
-    fun getPopularTvSeries(genreId: Int?) {
-        _homeUiState.update {
-            it.copy(
-                popularTvSeries = if (genreId != null) {
-                    seriesRepository.getPopularTvSeries().map { pagingData ->
-                        pagingData.filter {
-                            it.genreIds.contains(genreId)
-                        }
-                    }.cachedIn(viewModelScope)
-                } else {
-                    seriesRepository.getPopularTvSeries().cachedIn(viewModelScope)
-                }
-            )
-        }
-    }
-
-    private fun getSeriesGenres() {
-        viewModelScope.launch {
-            when (val result = getTvSeriesGenresUseCase()) {
-                is Resource.Success -> {
-                    _homeUiState.update {
-                        it.copy(
-                            tvSeriesGenres = result.data ?: emptyList()
-                        )
-                    }
-                }
-
-                is Resource.Error -> {
-                    //loadingError.value = result.message.toString()
-                }
-
-                else -> {}
-            }
-        }
-    }
-
-    fun setGenre(genre: Genre?) {
-        _homeUiState.update {
-            it.copy(
-                selectedGenre = genre
-            )
-        }
-    }
-
-    fun setSelectedOption(item: String) {
-        _homeUiState.update {
-            it.copy(
-                selectedFilmOption = item
-            )
-        }
-    }
-
-    fun refreshAllData() {
-        getSeriesGenres()
-        getMoviesGenres()
-        getTrendingMovies(homeUiState.value.selectedGenre?.id)
-        getNowPayingMovies(homeUiState.value.selectedGenre?.id)
-        getUpcomingMovies(homeUiState.value.selectedGenre?.id)
-        getTopRatedMovies(homeUiState.value.selectedGenre?.id)
-        getPopularMovies(homeUiState.value.selectedGenre?.id)
-        getPopularTvSeries(homeUiState.value.selectedGenre?.id)
-        getAiringTodayTvSeries(homeUiState.value.selectedGenre?.id)
-        getTrendingTvSeries(homeUiState.value.selectedGenre?.id)
-        getOnTheAirTvSeries(homeUiState.value.selectedGenre?.id)
-        getTopRatedTvSeries(homeUiState.value.selectedGenre?.id)
-        getOnTheAirTvSeries(homeUiState.value.selectedGenre?.id)
-    }
+    @GET("search/multi")
+    suspend fun multiSearch(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("query") query: String,
+        @Query("api_key") apiKey: String = API_KEY
+    ): MultiSearchResponse
 }
