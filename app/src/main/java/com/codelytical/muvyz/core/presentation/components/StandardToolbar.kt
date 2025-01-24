b0c593bb55c88c17615eea4a4b8d1bf66ac279c8
Keo vanpay <keovannpay582@gmail.com>
2025-01-24 15:35:26 +0700

    Fix toolbars

--- app/src/main/java/com/codelytical/muvyz/core/presentation/components/StandardToolbar.kt
+++ app/src/main/java/com/codelytical/muvyz/core/presentation/components/StandardToolbar.kt
@@ -14,7 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.codelytical.muvyz.core.presentation.theme.MoviewTheme
-
+//toolbars
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StandardToolbar(
